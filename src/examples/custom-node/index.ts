56ae4c808d86d051942481c7bbe57dd3068293c9
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-02-09 11:17:09 +0000

    hello world and custom node

---
+++ src/examples/custom-node/index.ts
@@ -0,0 +1,19 @@
+import { code, board } from "@google-labs/breadboard";
+import { merMake } from "../../util/merMake.js";
+
+const greet = code<{ name: string }>(({ name }) => {
+  const greeting = "Hello ".concat(name).concat("!");
+  return { greeting };
+});
+
+const myBoard = board<{ name: string }>(({ name }) => {
+  const greetNode = greet({ name });
+  return { greeting: greetNode.greeting };
+});
+
+console.log(JSON.stringify(await myBoard({ name: "Jim" }), null, 2));
+
+await merMake({
+  graph: myBoard,
+  destination: import.meta.dirname,
+});
