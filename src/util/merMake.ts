348561369d0d16416f5048cf53fd717a1ede3e00
Joseph Mearman <joseph@mearman.co.uk>
2024-02-23 16:59:17 +0000

    Add metadata support to merMake function

--- src/util/merMake.ts
+++ src/util/merMake.ts
@@ -1,9 +1,10 @@
-import { GraphDescriptor, Lambda, toMermaid } from "@google-labs/breadboard";
+import { GraphDescriptor, GraphMetadata, Lambda, toMermaid } from "@google-labs/breadboard";
 import fs from "fs";
 import path from "path";
 
-export async function merMake({ graph, destination: output = process.cwd() }: {
+export async function merMake({ graph, metadata, destination: output = process.cwd() }: {
 	graph: GraphDescriptor | Lambda<any, any>,
+	metadata?: GraphMetadata,
 	destination?: string;
 }) {
 	if (fs.lstatSync(output).isDirectory()) {
@@ -14,7 +15,7 @@ export async function merMake({ graph, destination: output = process.cwd() }: {
 	output = path.resolve(output);
 
 	if (typeof graph === "function") {
-		graph = await graph.serialize();
+		graph = await graph.serialize(metadata);
 	}
 	const mermaid = toMermaid(graph);
 	const condeFence = ["## Mermaid", "```mermaid", mermaid, "```"];
@@ -22,7 +23,9 @@ export async function merMake({ graph, destination: output = process.cwd() }: {
 	const jsonCodeFence = ["## JSON", "```json", json, "```"];
 	console.debug("Writing to", output);
 	fs.writeFileSync(output, [
+		metadata?.title ? `# ${metadata.title}` : undefined,
+		metadata?.description ? `> ${metadata.description}` : undefined,
 		condeFence.join("\n"),
 		jsonCodeFence.join("\n"),
-	].join("\n\n"));
+	].filter(Boolean).join("\n\n"));
 }
