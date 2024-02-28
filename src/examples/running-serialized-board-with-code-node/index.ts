7724e29bba4410e7de3891f5e4d9318a8bba5f88
tina.kingsley-hughes <tina.kingsley-hughes@sourcepulp.com>
2024-02-28 09:48:45 +0000

    Sample running a serialised board that has a code node

---
+++ src/examples/running-serialized-board-with-code-node/index.ts
@@ -0,0 +1,62 @@
+import {
+	BoardRunner,
+	OutputValues,
+	asRuntimeKit,
+	board,
+	code,
+} from "@google-labs/breadboard";
+import { merMake } from "../../util/merMake.js";
+import Core from "@google-labs/core-kit";
+
+// Define code node outside board
+const toUppercase = code<{ input: string }, OutputValues>(({ input }) => {
+	const uppercase = input.toUpperCase();
+	return { uppercase };
+});
+
+const myBoard = board<{ greet: string; subject: string }, OutputValues>(
+	({ greet, subject }) => {
+		const uppercase = toUppercase({ input: greet });
+
+		// Define code node inside board
+		const concat = code(({ base, toConcat }) => {
+			const concatenated = (base as string).concat(toConcat as string);
+			return { concatenated };
+		})();
+
+		greet.as("base").to(concat);
+		subject.as("toConcat").to(concat);
+
+		// Anonymous code node
+		const reversed = concat.concatenated.as("text").to(
+			code(({ text }) => {
+				const reversed = (text as string).split("").reverse().join("");
+				return { reversed };
+			})()
+		);
+
+		return {
+			uppercase: uppercase,
+			concatenated: concat.concatenated,
+			reversed: reversed.reversed,
+		};
+	}
+);
+
+const runner = await BoardRunner.fromGraphDescriptor(
+	await myBoard.serialize({ title: "My Board" })
+);
+
+console.log(
+	JSON.stringify(
+		await runner.runOnce(
+			{ greet: "Hello", subject: "World" },
+			{ kits: [asRuntimeKit(Core)] }),
+		null,
+		2
+	)
+);
+await merMake({
+	graph: myBoard,
+	destination: import.meta.dirname,
+});
