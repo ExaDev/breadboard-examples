5ff394fa55a111d7261a606c2d1929b0e1c6de26
jig925 <57284752+jig925@users.noreply.github.com>
2024-03-27 08:47:45 +0000

    WIP

--- src/examples/hackerNews/SchemaBug.ts
+++ src/examples/hackerNews/SchemaBug.ts
@@ -4,7 +4,7 @@ import Core from "@google-labs/core-kit";
 const stringInputSchema = {
     type: "string",
     title: "My String",
-    default: "myString",
+    default: "default",
     description: "String to concat",
 };
 
@@ -17,11 +17,12 @@ const myBoard = await board(() => {
     const input = base.input({
         $id: "concat",
         schema: {
-            title: "Hacker News Story",
+            title: "Concat Inputs",
             properties: {
                 string1: stringInputSchema,
                 string2: stringInputSchema
             },
+            required: ["string2"]
         },
     })
 
@@ -30,21 +31,32 @@ const myBoard = await board(() => {
     return { output }
 }).serialize(
     {
-        title: "Hacker News Firebase API Story Extractor",
-        description: "Board which extracts contents of a given Hacker News story ID",
+        title: "Schema Bug",
+        description: "Board which demonstrates schema defaults not being used on required properties.",
         version: "0.0.1"
     }
 )
 
 const kits = [asRuntimeKit(Core)]
 
-// this SHOULD return "1myString" because we are using a default, but it does not return the result as expected
+// this SHOULD return "This is default" because we are using a default, but it does not return the result as expected
 const runner = await BoardRunner.fromGraphDescriptor(myBoard);
 for await (const stop of runner.run({ kits: kits })) {
     if (stop.type === "input") {
         stop.inputs = {
             string1: "This is ",
-            // string2: "Ok"
+        };
+    } else if (stop.type === "output") {
+        console.log(stop.outputs)
+    }
+}
+
+const runner2 = await BoardRunner.fromGraphDescriptor(myBoard);
+for await (const stop of runner2.run({ kits: kits })) {
+    if (stop.type === "input") {
+        stop.inputs = {
+            string1: "This is ",
+            string2: "not the default"
         };
     } else if (stop.type === "output") {
         console.log(stop.outputs)
