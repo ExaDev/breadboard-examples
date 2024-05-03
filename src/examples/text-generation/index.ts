196f7d6235317a0bdf6ce83724a77dec250b0403
jig925 <57284752+jig925@users.noreply.github.com>
2024-05-03 15:31:34 +0100

    Updated Model
    Updated model used by text generation

--- src/examples/text-generation/index.ts
+++ src/examples/text-generation/index.ts
@@ -31,82 +31,27 @@ const topPSchema = {
     description: "Float to define the tokens that are within the sample operation of text generation. Add tokens in the sample for more probable to least probable until the sum of the probabilities is greater than top_p"
 };
 
-const temperatureSchema = {
+const maxLengthSchema = {
     type: "number",
-    title: "temperature",
-    default: "1.0",
-    description: "The temperature of the sampling operation. 1 means regular sampling, 0 means always take the highest score, 100.0 is getting closer to uniform probability"
-};
-
-const repetitionPenaltySchema = {
-    type: "number",
-    title: "repetition_penalty",
-    default: "1.0",
-    description: "The more a token is used within generation the more it is penalized to not be picked in successive generation passes"
-};
-
-const maxNewTokensSchema = {
-    type: "number",
-    title: "max_tokens",
-    default: "None",
-    description: "The amount of new tokens to be generated, this does not include the input length it is a estimate of the size of generated text you want. Each new tokens slows down the request, so look for balance between response times and length of text generated"
-};
-
-const maxTimeSchema = {
-    type: "number",
-    title: "max_time",
-    default: "None",
-    description: "The amount of time in seconds that the query should take maximum. Network can cause some overhead so it will be a soft limit. Use that in combination with max_new_tokens for best results"
-};
-
-const returnFullTextSchema = {
-    type: "boolean",
-    title: "return_full_text",
-    default: "true",
-    description: "Bool. If set to False, the return results will not contain the original query making it easier for prompting"
+    title: "max_length",
+    default: "100",
+    description: "Max Length of the input"
 };
 
 const numReturnSequencesSchema = {
     type: "number",
     title: "num_return_sequences",
-    default: "1",
-};
+    default: "3",
+    description: "Number of responses "
+}
 
-const doSampleSchema = {
-    type: "boolean",
-    title: "do_sample",
-    default: "true",
-    description: "Bool. Whether or not to use sampling, use greedy decoding otherwise"
-};
-
-const useCacheSchema = {
-    type: "boolean",
-    title: "use_cache",
-    default: "true",
-    description: "Boolean. There is a cache layer on the inference API to speedup requests we have already seen. Most models can use those results as is as models are deterministic (meaning the results will be the same anyway). However if you use a non deterministic model, you can set this parameter to prevent the caching mechanism from being used resulting in a real new query"
-};
-
-const waitForModelSchema = {
-    type: "boolean",
-    title: "wait_for_model",
-    default: "false",
-    description: " Boolean. If the model is not ready, wait for it instead of receiving 503. It limits the number of requests required to get your inference done. It is advised to only set this flag to true after receiving a 503 error as it will limit hanging in your application to known places"
-};
 
 export type HuggingFaceTextGenerationRawParams = {
     inputs: string
     top_k: number;
     top_p: number;
-    temperature: number;
-    repetition_penalty: number;
-    max_new_tokens: number;
-    max_time: number;
-    return_full_text: boolean;
+    max_length: number;
     num_return_sequences: number;
-    do_sample: boolean
-
-    use_cache: boolean
-    wait_for_model: boolean
 };
 
 export type HuggingFaceTextGenerationParams = {
@@ -114,21 +59,10 @@ export type HuggingFaceTextGenerationParams = {
     parameters: {
         top_k: number;
         top_p: number;
-        temperature: number;
-        repetition_penalty: number;
-        max_new_tokens: number;
-        max_time: number;
-        return_full_text: boolean;
+        max_length: number;
         num_return_sequences: number;
-        do_sample: boolean
-
-        options: {
-            use_cache: boolean
-            wait_for_model: boolean
-        }
     }
 };
-
 
 const authenticate = code<{ key: string }>((inputs) => {
     const key = inputs.key
@@ -137,42 +71,24 @@ const authenticate = code<{ key: string }>((inputs) => {
     return { auth };
 });
 
-const handleParams = code<{ inputs: string, top_k: number, top_p: number, temperature: number, repetition_penalty: number, max_new_tokens: number, max_time: number, return_full_text: boolean, num_return_sequences: number, do_sample: boolean, use_cache: boolean, wait_for_model: boolean }>((input) => {
+const handleParams = code<{ inputs: string, top_k: number, top_p:number, max_length: number, num_return_sequences: number}>((input) => {
     const {
         inputs,
         top_k,
         top_p,
-        temperature,
-        repetition_penalty,
-        max_new_tokens,
-        max_time,
-        return_full_text,
-        num_return_sequences,
-        do_sample,
-        use_cache,
-        wait_for_model
+        max_length,
+        num_return_sequences
     } = input
 
-    const request: HuggingFaceTextGenerationParams = {
+    const payload: HuggingFaceTextGenerationParams = {
         inputs: inputs,
         parameters: {
             top_k: top_k,
             top_p: top_p,
-            temperature: temperature,
-            repetition_penalty: repetition_penalty,
-            max_new_tokens: max_new_tokens,
-            max_time: max_time,
-            return_full_text: return_full_text,
-            num_return_sequences: num_return_sequences,
-            do_sample: do_sample,
-            options: {
-                use_cache: use_cache,
-                wait_for_model: wait_for_model
-            }
+            max_length: max_length,
+            num_return_sequences: num_return_sequences
         }
     }
-
-    const payload = JSON.stringify(request)
 
     return { payload }
 })
@@ -187,21 +103,13 @@ const serialized = await board(() => {
                 apiKey: keySchema,
                 top_k: topKSchema,
                 top_p: topPSchema,
-                temperature: temperatureSchema,
-                repetition_penalty: repetitionPenaltySchema,
-                max_new_tokens: maxNewTokensSchema,
-                max_time: maxTimeSchema,
-                return_full_text: returnFullTextSchema,
-                num_return_sequences: numReturnSequencesSchema,
-                do_sample: doSampleSchema,
-                use_cache: useCacheSchema,
-                wait_for_model: waitForModelSchema
+                max_length: maxLengthSchema
             },
         },
         type: "string",
     });
 
-    const task = "https://api-inference.huggingface.co/models/gpt2"
+    const task = "https://api-inference.huggingface.co/models/openai-community/gpt2"
     const output = base.output({ $id: "main" });
 
     const { auth } = authenticate({ key: inputs.apiKey as unknown as string })
@@ -209,15 +117,8 @@ const serialized = await board(() => {
         inputs: inputs.inputs as unknown as string,
         top_k: inputs.top_k as unknown as number,
         top_p: inputs.top_p as unknown as number,
-        temperature: inputs.temperature as unknown as number,
-        repetition_penalty: inputs.repetition_penalty as unknown as number,
-        max_new_tokens: inputs.max_new_tokens as unknown as number,
-        max_time: inputs.max_time as unknown as number,
-        return_full_text: inputs.return_full_text as unknown as boolean,
-        num_return_sequences: inputs.num_return_sequences as unknown as number,
-        do_sample: inputs.do_sample as unknown as boolean,
-        use_cache: inputs.use_cache as unknown as boolean,
-        wait_for_model: inputs.wait_for_model as unknown as boolean
+        max_length: inputs.max_length as unknown as number,
+        num_return_sequences: inputs.num_return_sequences as unknown as number
     });
 
     const response = core.fetch({
@@ -234,7 +135,6 @@ const serialized = await board(() => {
     title: "Hugging Face Text Generation Board",
     description: "Board which calls the Hugging Face Text Generation Endpoint"
 });
-
 
 fs.writeFileSync(
     path.join(".", "board.json"),
