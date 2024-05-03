d52e60d4baed66a2c566811772e42447c6af5bc0
jig925 <57284752+jig925@users.noreply.github.com>
2024-05-03 14:24:11 +0100

    Fix

--- src/examples/sentence-similarity/index.ts
+++ src/examples/sentence-similarity/index.ts
@@ -11,7 +11,7 @@ const soruceSentenceSchema = {
 };
 
 const sentencesSchema = {
-    type: "array",
+    type: "list",
     title: "sentences",
     default: "[That is a happy dog, That is a very happy person,Today is a sunny day]",
     description: "A list of sentences to compare the source sentence to"
