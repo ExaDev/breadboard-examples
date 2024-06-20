b949a35fb07c867191e1a630ea17f9edf949c4bf
jig925 <57284752+jig925@users.noreply.github.com>
2024-06-20 08:50:07 +0100

    Update index.ts

--- src/examples/summarization/index.ts
+++ src/examples/summarization/index.ts
@@ -17,69 +17,6 @@ const keySchema = {
     description: "The hugging face api key"
 };
 
-const minLengthSchema = {
-    type: "string",
-    title: "number",
-    default: "None",
-    description: "Integer to define the minimum length in tokens of the output summary"
-};
-
-const maxLengthSchema = {
-    type: "number",
-    title: "max_length",
-    default: "None",
-    description: "Integer to define the maximum length in tokens of the output summary"
-};
-
-const topKSchema = {
-    type: "number",
-    title: "top_k",
-    default: "None",
-    description: "Integer to define the top tokens considered within the sample operation to create new text"
-};
-
-const topPSchema = {
-    type: "number",
-    title: "top_P",
-    default: "None",
-    description: "Float to define the tokens that are within the sample operation of text generation. Add tokens in the sample for more probable to least probable until the sum of the probabilities is greater than top_p"
-};
-
-const temperatureSchema = {
-    type: "number",
-    title: "temperature",
-    default: "1.0",
-    description: "The temperature of the sampling operation. 1 means regular sampling, 0 means always take the highest score, 100.0 is getting closer to uniform probability"
-};
-
-const repetitionPenaltySchema = {
-    type: "number",
-    title: "repetition_penalty",
-    default: "1.0",
-    description: "The more a token is used within generation the more it is penalized to not be picked in successive generation passes"
-};
-
-const maxTimeSchema = {
-    type: "number",
-    title: "max_time",
-    default: "None",
-    description: "The amount of time in seconds that the query should take maximum. Network can cause some overhead so it will be a soft limit"
-};
-
-const useCacheSchema = {
-    type: "boolean",
-    title: "use_cache",
-    default: "true",
-    description: "Boolean. There is a cache layer on the inference API to speedup requests we have already seen. Most models can use those results as is as models are deterministic (meaning the results will be the same anyway). However if you use a non deterministic model, you can set this parameter to prevent the caching mechanism from being used resulting in a real new query"
-};
-
-const waitForModelSchema = {
-    type: "boolean",
-    title: "wait_for_model",
-    default: "false",
-    description: " Boolean. If the model is not ready, wait for it instead of receiving 503. It limits the number of requests required to get your inference done. It is advised to only set this flag to true after receiving a 503 error as it will limit hanging in your application to known places"
-};
-
 export type HuggingFaceSummarizationParams = {
     inputs: string
     parameters: {
@@ -132,7 +69,7 @@ const serialized = await board(() => {
 
     const { auth } = authenticate({ key: inputs.apiKey as unknown as string });
     const { payload } = handleParams({
-        inputs: inputs.inputs as unknown as string,
+        inputs: inputs.inputs.isString(),
     });
 
     const response = core.fetch({
